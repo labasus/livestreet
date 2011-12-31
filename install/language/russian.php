e122c669f7214cccc5050488812e8dcbe3088b5f
labasus <profi.admin@gmail.com>
2011-12-31 14:48:17 +0200

    Update install/language/russian.php

--- install/language/russian.php
+++ install/language/russian.php
@@ -55,7 +55,7 @@ return array(
 	'install_title'=>'Установка LiveStreet',
 	'step'=>'Шаг',
 	
-	'start_paragraph'=>'<p>Добро пожаловать в инсталлятор LiveStreet &copy; 0.5.1. Ознакомьтесь с результатами и следуйте подсказкам.</p><p><b>Внимание.</b> Для успешной иснталяции вы должны переименовать файл /config/config.local.php.dist на config.local.php и дать этому файлу права на запись.</p><p><b>Внимание.</b> Директории /tmp, /logs, /uploads, /templates/compiled, /templates/cache должны иметь права на запись.</p>',
+	'start_paragraph'=>'<p>Добро пожаловать в инсталлятор LiveStreet &copy; 0.5.1. Ознакомьтесь с результатами и следуйте подсказкам.</p><p><b>Внимание.</b> Для успешной инсталяции вы должны переименовать файл /config/config.local.php.dist на config.local.php и дать этому файлу права на запись.</p><p><b>Внимание.</b> Директории /tmp, /logs, /uploads, /templates/compiled, /templates/cache должны иметь права на запись.</p>',
 	
 	'php_params'=>'Основные настройки PHP',
 	'php_params_version'=>'PHP версии не ниже 5.2.0',
